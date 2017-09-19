8e23e2623b77bd7bdcc9379e6a7b61570bcd758b
tontonton <phantom.ness@windowslive.com>
2017-09-19 16:54:49 +0700

    Add vue js

---
+++ src/components/App/script.js
@@ -0,0 +1,8 @@
+import Hello from '../Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
---
+++ src/components/Hello/index.vue
@@ -0,0 +1,5 @@
+<template src="./template.html"></template>
+
+<script src="./script.js"></script>
+
+<style scoped src="./style.css"></style>
